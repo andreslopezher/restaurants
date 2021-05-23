9f5317d461bdacc239b1620c56440242372ac9a7
Andres Lopez <andreslopezher@gmail.com>
2021-05-23 16:09:21 -0500

    Add navigation

---
+++ screens/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Seach() {
+    return (
+        <View>
+            <Text>Seach</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
