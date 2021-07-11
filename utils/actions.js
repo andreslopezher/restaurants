661c5fa6e5deeb265f024aa4c5d28e0f619389a6
Andres Lopez <andreslopezher@gmail.com>
2021-07-11 17:53:17 -0500

    add comments to code
    comments

--- utils/actions.js
+++ utils/actions.js
@@ -2,8 +2,10 @@ import { firebaseApp } from './firebase'
 import * as firebase from 'firebase'
 import 'firebase/firestore'
 
+//Tener acceso a la base de datos.
 const db = firebase.firestore(firebaseApp)
 
+//Función para indicar si el usuario esta logueado
 export const isUserLogged = () => {
     let isLogged = false
     firebase.auth().onAuthStateChanged((user) => {
