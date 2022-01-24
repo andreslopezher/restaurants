611c1d7e967574b716e1df2091bc37dc5150520d
Andres Lopez <andreslopezher@gmail.com>
2022-01-23 20:19:02 -0600

    Navigate to login page
    Login page creation.

--- screens/account/Account.js
+++ screens/account/Account.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect , useState } from 'react'
+import { StyleSheet } from 'react-native'
 import firebase from 'firebase/app'
 
 import UserGuest from './UserGuest'
 import UserLogged from './UserLogged'
+import { getCurrentUser, isUserLogged } from '../../utils/actions'
+import Loading from '../../components/Loading'
 
 export default function Account() {
     const [login, setLogin] = useState(null)
 
-    firebaseApp.auth().onAuthStateChanged((user) => {
-        user !== null ? (setLogin(true)) : setLogin(false)
-    })
+    useEffect(() => {
+        //const user = getCurrentUser()
+        //user ? setLogin(true) : setLogin(false) 
+        setLogin(isUserLogged())
+    }, [])
 
     if (login == null){
-        return <Text>Cargando...</Text>
+        return <Loading isVisible={true} text="Cargando..."/>
     }
 
     return login ? <UserLogged/> : <UserGuest/>
--- screens/account/Login.js
+++ screens/account/Login.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function Account() {
+export default function Login() {
     return (
         <View>
-            <Text>Accounts</Text>
+            <Text>Login</Text>
         </View>
     )
 }
