5f8b1bfc34ffe29a9edf10388ea66700eccca7c2
Andres Lopez <andreslopezher@gmail.com>
2022-01-23 22:32:56 -0600

    create register form page

---
+++ screens/account/Register.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import RegisterForm from '../../components/account/RegisterForm'
+
+const Register = () => {
+    return (
+        <View>
+            <RegisterForm/>
+        </View>
+    )
+}
+
+export default Register
+
+const styles = StyleSheet.create({})
