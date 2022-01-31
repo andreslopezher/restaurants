e1a4b8765db42c3aa348aef1b4cdaae896eb6e03
Andres Lopez <andreslopezher@gmail.com>
2022-01-30 22:27:18 -0600

    Add KeyboardAwareScrollView and password eye icon
    1.- Add keyboard to avoid input hiden.
    2.- Add eye icon to enable to see or not the password.

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Button, Input } from 'react-native-elements'
+import { Button, Input, Icon } from 'react-native-elements'
 
 export default function RegisterForm() {
+    const [showPassword, setShowPassword] = useState(false) 
+
     return (
         <View style={styles.form}>
             <Input 
@@ -13,13 +15,29 @@ export default function RegisterForm() {
                 containerStyle={styles.input}
                 placeholder="Ingresa tu contraseña..."
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={!showPassword}
+                rightIcon={
+                    <Icon
+                        type="material-community"
+                        name={ showPassword ? "eye-off-outline" : "eye-outline"}
+                        iconStyle={styles.icon}
+                        onPress={() => setShowPassword(!showPassword)}
+                    />
+                }
             />
             <Input 
                 containerStyle={styles.input}
                 placeholder="Confirma tu contraseña..."
                 password={true}
-                secureTextEntry={true}
+                secureTextEntry={!showPassword}
+                rightIcon={
+                    <Icon
+                    type="material-community"
+                    name={ showPassword ? "eye-off-outline" : "eye-outline"}
+                    iconStyle={styles.icon}
+                    onPress={() => setShowPassword(!showPassword)}    
+                    />
+                }
             />
             <Button
                 title="Registrar nuevo usuario"
@@ -44,5 +62,8 @@ const styles = StyleSheet.create({
     },
     btn : {
         backgroundColor: "#442484"
-    }
+    },
+    icon : {
+        color: "#C1C1C1"
+    } 
 })
--- screens/account/Register.js
+++ screens/account/Register.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import RegisterForm from '../../components/account/RegisterForm'
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import RegisterForm from "../../components/account/RegisterForm";
 
 const Register = () => {
-    return (
-        <View>
-            <RegisterForm/>
-        </View>
-    )
-}
+  return (
+    <KeyboardAwareScrollView>
+      <Image
+        source={require("../../assets/restaurant-logo.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <RegisterForm />
+    </KeyboardAwareScrollView>
+  );
+};
 
-export default Register
+export default Register;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({  
+    image : {
+        height: 150,
+        width: "100%",
+        marginBottom: 20
+    }
+});
