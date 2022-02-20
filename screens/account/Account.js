e3153d75d188ddbc522af1155a3050b8863e6b20
Andres Lopez <andreslopezher@gmail.com>
2022-02-19 23:20:19 -0600

    Validate to show user logged page

--- screens/account/Account.js
+++ screens/account/Account.js
@@ -1,20 +1,21 @@
-import React, { useEffect , useState } from 'react'
+import React, { useEffect , useState, useCallback } from 'react'
 import { StyleSheet } from 'react-native'
-import firebase from 'firebase/app'
+import Loading from '../../components/Loading'
+import { getCurrentUser, isUserLogged } from '../../utils/actions'
+import { useFocusEffect } from '@react-navigation/native'
 
 import UserGuest from './UserGuest'
 import UserLogged from './UserLogged'
-import { getCurrentUser, isUserLogged } from '../../utils/actions'
-import Loading from '../../components/Loading'
 
 export default function Account() {
     const [login, setLogin] = useState(null)
 
-    useEffect(() => {
-        //const user = getCurrentUser()
-        //user ? setLogin(true) : setLogin(false) 
-        setLogin(isUserLogged())
-    }, [])
+    useFocusEffect(
+        useCallback(() => {
+            const user = getCurrentUser()
+            user ? setLogin(true) : setLogin(false)
+        }, [])
+    )
 
     if (login == null){
         return <Loading isVisible={true} text="Cargando..."/>
